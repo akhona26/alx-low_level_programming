4b9de42a5ffd9638b691ea97dbbb81513ca2a092
Akhona <akhona.mtongana02@gmail.com>
2022-07-18 15:14:02 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
@@ -7,7 +6,7 @@
  * @n: number of times to copy b
  * Return: pointer to the memory area s
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
